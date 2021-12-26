ae13e062d28f34c00a6e7cfc2d6adeb0d8807c6e
Rochelle Maxwell <90586508+msmaxiii@users.noreply.github.com>
2021-12-25 20:26:25 -0500

    name function

---
+++ app.js
@@ -0,0 +1,54 @@
+let person = prompt("Please enter your name", "");
+
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+  "Hello " + person + "! How are you today?";
+}
+
+const USS_Schwarzenegger ={
+hull: 20,
+firepower: 5,
+accuracy: .7,
+attack: function(){if (Math.random()<alien[0].accuracy){
+    console.log('You have been hit!');
+    }
+}
+}
+const Enemy ={
+    hull:  3 <= 6,
+    firepower:  2 <= 4,
+    accuracy: .6 <= .8,
+
+}
+// class moreEnemy{
+    let i=0 
+//     constructor(hull,firepower,accuracy){
+//         this.hull = hull;
+//         this.firepower = firepower;
+//         this.accuracy = accuracy;
+//     }
+// }
+
+// class moreEnemy extends Enemy{
+//     constructor(hull,firepower,accuracy){
+//         super()
+//     }
+// }
+
+
+const startBtn=document.getElementById('start')
+const attackBtn=document.getElementById('attack')
+
+// for (let ussSpaceship = )
+
+
+
+
+
+
+
+
+
+
+// startBtn.addEventListner('click',start)
+// attackBtn.addEventListner('click',attack)
